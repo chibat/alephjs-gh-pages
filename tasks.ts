87f1aafcea2472bf35bbfae97f0ba6b17cc4925e
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2021-04-15 23:52:10 +0900

    deploy

--- tasks.ts
+++ tasks.ts
@@ -2,28 +2,11 @@
 
 import { existsSync } from "https://deno.land/std@0.93.0/fs/exists.ts";
 
-const text = await Deno.readTextFile("import_map.json");
-const alephPath = JSON.parse(text).imports["aleph/"];
+//
+// Tasks
+//
 
-const taskName = Deno.args[0];
-
-switch (taskName) {
-  case "build":
-    await build();
-    break;
-
-  case "deploy":
-    await build();
-    Deno.createSync("docs/.nojekyll");
-    await $("git add .");
-    await $("git commit -m deploy");
-    await $("git push");
-    break;
-
-  default:
-    console.log("Unknown task name");
-    break;
-}
+const TASKS = [build, dev, start, deploy];
 
 async function build() {
   rm(".aleph");
@@ -31,13 +14,73 @@ async function build() {
     Deno.execPath(),
     "run",
     "--allow-net=deno.land,esm.sh",
-    "--allow-read=.," + Deno.execPath() + "," + await getDenoDir(),
+    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
     "--allow-write=.aleph,docs",
     "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK",
     "--allow-run=" + Deno.execPath(),
-    `${alephPath}cli.ts`,
+    alephCliPath(),
     "build",
   ]);
+}
+
+async function deploy() {
+  await build();
+  Deno.createSync("docs/.nojekyll");
+  await $("git add .");
+  await $("git commit -m deploy");
+  await $("git push");
+}
+
+async function dev() {
+  await $([
+    Deno.execPath(),
+    "run",
+    "--allow-net=deno.land,esm.sh,:8080",
+    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
+    "--allow-write=.aleph",
+    "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK",
+    "--allow-run=" + Deno.execPath(),
+    alephCliPath(),
+    "dev",
+  ]);
+}
+
+async function start() {
+  await $([
+    Deno.execPath(),
+    "run",
+    "--allow-net=deno.land,esm.sh,:8080",
+    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
+    "--allow-write=.aleph",
+    "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK",
+    "--allow-run=" + Deno.execPath(),
+    alephCliPath(),
+    "start",
+  ]);
+}
+
+//
+// Aleph.js
+//
+
+function alephCliPath() {
+  return JSON.parse(Deno.readTextFileSync("import_map.json"))
+    .imports["aleph/"] + "cli.ts";
+}
+
+//
+// Utility
+//
+
+async function denoDir() {
+  const p = Deno.run({
+    cmd: [Deno.execPath(), "info", "--json", "--unstable"],
+    stdout: "piped",
+    stderr: "null",
+  });
+  const output = (new TextDecoder()).decode(await p.output());
+  p.close();
+  return JSON.parse(output).denoDir;
 }
 
 async function $(cmd: string[] | string) {
@@ -58,13 +101,21 @@ function rm(path: string) {
   }
 }
 
-async function getDenoDir() {
-  const p = Deno.run({
-    cmd: [Deno.execPath(), "info", "--json", "--unstable"],
-    stdout: "piped",
-    stderr: "null",
-  });
-  const output = (new TextDecoder()).decode(await p.output());
-  p.close();
-  return JSON.parse(output).denoDir;
+//
+// main
+//
+
+if (import.meta.main) {
+  const taskName = Deno.args[0];
+  if (!taskName) {
+    TASKS.forEach((task) => console.log(task.name));
+    Deno.exit(0);
+  }
+  const tasks = TASKS.filter((task) => task.name === taskName);
+  if (tasks.length == 0) {
+    console.log("Unknown task name");
+    Deno.exit(1);
+  }
+
+  tasks.find((task) => task());
 }
